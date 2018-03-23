1955eb2690f01e471ea6847c39d6f5f64980c03d
jonobral <jonobral@gmail.com>
2018-03-22 22:24:47 -0600

    commit version_03_22

--- src/components/JobsHistory/JobsHistory.js
+++ src/components/JobsHistory/JobsHistory.js
@@ -6,7 +6,6 @@ class JobsHistory extends Component {
   render() {
     return (
       <div className="jobs-history">
-        <span>I started as front-end developer the day Pinto came to my home...7 years ago</span>
       </div>
     );
   }
